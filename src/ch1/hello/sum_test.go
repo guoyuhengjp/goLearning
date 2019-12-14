14c9afc67b2bb78615c9a6a8a752f83f61d85e5c
郭宇恒 <guoyuhengjp@gmail.com>
2019-12-14 20:14:37 +0900

    let code 1

---
+++ src/ch1/hello/sum_test.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func twoSum(nums []int, target int) []int {
+
+	var result []int
+
+	for i:= 0;i < len(nums); i++{
+		for j := i + 1;j < len(nums); j++{
+			fmt.Println(i)
+			fmt.Println(j)
+			if nums[i] + nums[j] == target{
+				result = append(result,i,j)
+			}
+		}
+		fmt.Println(i)
+	}
+
+	return result
+}
+
+func TestSum(t *testing.T) {
+	nums := []int{3,2,4}
+
+	target := 6
+
+	result :=twoSum(nums,target)
+
+	fmt.Println(result)
+}
+
