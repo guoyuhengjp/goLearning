f0e794058ff005d78ccb25b061678d5f99fccb4b
郭宇恒 <guoyuhengjp@gmail.com>
2019-12-14 10:40:42 +0900

    recover from erro

---
+++ src/ch14/panic_recover/panic_recover_test.go
@@ -0,0 +1,21 @@
+package panic_recover
+
+import (
+	"errors"
+	"fmt"
+	"testing"
+)
+
+func TestPanicVxExit(t *testing.T) {
+
+	defer func(){
+		if err := recover(); err != nil {
+			fmt.Println("recovered from ",err)
+		}}()
+
+	fmt.Println("start")
+	panic(errors.New("something wrong"))
+}
+
+
+
