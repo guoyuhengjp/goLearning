5d0a39f441b3b549b3236841dae763c2381a86ca
guoyuhengjp <guoyuhengjp@gmail.com>
2020-02-05 10:53:39 +0900

    整数反转

--- src/letcode/twosum/otherAns_test.go
+++ src/letcode/twosum/otherAns_test.go
@@ -46,3 +46,26 @@ func TestTwo(t *testing.T) {
 
 	fmt.Println(result)
 }
+
+
+func reverse(x int) int {
+
+	var result int = 0
+
+	for ; x !=0; x/=10  {
+		result = result*10 + x % 10
+
+		if result > 2147483647|| result < -2147483648{
+			return 0
+		}
+	}
+	return result
+}
+
+func TestReverse(t *testing.T) {
+	var x int = 1534236469
+
+	result := reverse(x)
+
+	println(result)
+}
